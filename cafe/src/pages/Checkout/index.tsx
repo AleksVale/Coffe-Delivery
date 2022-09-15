f1f6986bf56dbc852e550b5c9852428ce8c06f5e
Aleksander Ribeiro Vale <aleksander.ribeiro@sga.pucminas.br>
2022-09-15 14:13:23 -0300

    configurando checkout

--- cafe/src/pages/Checkout/index.tsx
+++ cafe/src/pages/Checkout/index.tsx
@@ -1,10 +1,26 @@
-import { LayoutCheckout } from "./styles";
+import { AdressForm, CheckoutInfos, HeaderForm, LayoutCheckout } from "./styles";
+import { MapPinLine } from "phosphor-react";
 
 export function Checkout(){
 
   return(
     <LayoutCheckout>
-      <div>Complete seu pedido</div>
+      <div>
+        <h3>Complete seu pedido</h3>
+        <CheckoutInfos>
+         <HeaderForm> 
+            <p><span><MapPinLine size={22} /></span> Endereço de Entrega</p>
+            <p>Informe o endereço onde deseja receber seu pedido</p>
+          </HeaderForm>
+          <AdressForm>
+            <p><input type="text" placeholder="CEP"/></p>
+            <p><input type="text" placeholder="Rua"/></p>
+            <p><input type="text" placeholder="Número"/>  <input type="text" placeholder="Complemento" /></p>
+            <p><input type="text" placeholder="Bairro" /> <input type="text" placeholder="Cidade"/> <input type="text" placeholder="UF"/></p>
+          </AdressForm>
+        </CheckoutInfos>
+        
+      </div>
       <div>mundo</div>
     </LayoutCheckout>
   )
