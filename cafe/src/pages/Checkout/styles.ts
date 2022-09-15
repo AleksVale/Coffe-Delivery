120d6f7fb570ead0297de6f71d611820e5f7a07e
Aleksander Ribeiro Vale <aleksander.ribeiro@sga.pucminas.br>
2022-09-15 14:13:23 -0300

    configurando checkout

--- cafe/src/pages/Checkout/styles.ts
+++ cafe/src/pages/Checkout/styles.ts
@@ -5,3 +5,49 @@ export const LayoutCheckout = styled.div`
   padding: 0 10rem;
   gap: 2rem;
 `
+export const CheckoutInfos = styled.div`
+  margin-top: 1rem;
+  padding: 2.5rem 2.5rem 2rem;
+  background-color: ${props => props.theme["base-card"]};
+  
+  
+`
+export const HeaderForm = styled.div`
+margin-bottom: 2rem;
+
+p:first-child{
+    color: ${props=> props.theme["base-subtitle"]};
+    display: flex;
+    align-items: center;
+    gap:0.5rem;
+    font-size: 16px;
+    line-height: 130%;
+    span{
+      color: ${props=> props.theme["yellow-dark"]};
+      width: 22px;
+      height:22px;
+    }
+  }
+  p:last-child{
+    font-size: 0.875rem;
+    color: ${props=> props.theme["base-text"]};
+    padding-left: 1.875rem;
+  }
+`
+export const AdressForm = styled.form`
+  
+
+  input{
+    background-color: ${props=> props.theme["base-input"]};
+    border: none;
+    height: 2.625rem;
+    margin-bottom: 1rem;
+    padding: 0.725rem;
+    font-size: 0.875rem;
+    color: ${props=>props.theme["base-text"]};
+    border-radius: 6px;
+  }
+  input::placeholder{
+    color: ${props=> props.theme["base-label"]};
+  }
+`
