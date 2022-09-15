353d6f9f35022065aaabc1415812ed3ae2b11677
Aleksander <alexalexx3@gmail.com>
2022-09-15 17:22:13 -0300

    checkout left ready

--- cafe/src/pages/Checkout/index.tsx
+++ cafe/src/pages/Checkout/index.tsx
@@ -1,5 +1,5 @@
-import { AdressForm, CheckoutInfos, HeaderForm, LayoutCheckout } from "./styles";
-import { MapPinLine } from "phosphor-react";
+import { AdressForm, CheckoutInfos, First, HeaderForm, LayoutCheckout, PaymentForm, Second, TypePayment } from "./styles";
+import { MapPinLine, CurrencyDollar, CreditCard, Bank, Money } from "phosphor-react";
 
 export function Checkout(){
 
@@ -8,16 +8,41 @@ export function Checkout(){
       <div>
         <h3>Complete seu pedido</h3>
         <CheckoutInfos>
+          <First>
          <HeaderForm> 
             <p><span><MapPinLine size={22} /></span> Endereço de Entrega</p>
             <p>Informe o endereço onde deseja receber seu pedido</p>
           </HeaderForm>
-          <AdressForm>
-            <p><input type="text" placeholder="CEP"/></p>
-            <p><input type="text" placeholder="Rua"/></p>
-            <p><input type="text" placeholder="Número"/>  <input type="text" placeholder="Complemento" /></p>
-            <p><input type="text" placeholder="Bairro" /> <input type="text" placeholder="Cidade"/> <input type="text" placeholder="UF"/></p>
-          </AdressForm>
+            <AdressForm>
+              <p><input type="text" placeholder="CEP" required/></p>
+              <p><input type="text" placeholder="Rua" required/></p>
+              <p><input type="text" placeholder="Número" required/>  <input type="text" placeholder="Complemento" /></p>
+              <p><input type="text" placeholder="Bairro" required/> <input type="text" placeholder="Cidade" required/> <input type="text" required placeholder="UF"/></p>
+            </AdressForm>
+          </First>
+          <Second>
+            <PaymentForm>
+              <p><span><CurrencyDollar size={22} weight="fill"/></span> <span>Pagamento</span></p>
+              <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+            </PaymentForm>
+            <TypePayment>
+              <div>
+               <label htmlFor="cartao"><span><CreditCard size={20} weight="thin" /></span>
+                CARTÃO DE CRÉDITO</label>
+                <input type="radio" id="cartao" value="cartao" name="pagamento"/>
+              </div>
+              <div>
+               <label htmlFor="debito"><span><Bank size={20} weight="thin" /></span>
+                CARTÃO DE DÉBITO</label>
+                <input type="radio" id="debito" value="debito" name="pagamento"/>
+              </div>
+              <div>
+               <label htmlFor="dinheiro"><span><Money size={20} weight="thin" /></span>
+                DINHEIRO</label>
+                <input type="radio" id="dinheiro" value="dinheiro" name="pagamento"/>
+              </div>
+            </TypePayment>
+          </Second>
         </CheckoutInfos>
         
       </div>
--- cafe/src/pages/Checkout/styles.ts
+++ cafe/src/pages/Checkout/styles.ts
@@ -5,12 +5,22 @@ export const LayoutCheckout = styled.div`
   padding: 0 10rem;
   gap: 2rem;
 `
-export const CheckoutInfos = styled.div`
-  margin-top: 1rem;
-  padding: 2.5rem 2.5rem 2rem;
-  background-color: ${props => props.theme["base-card"]};
+export const CheckoutInfos = styled.form`
   
-  
+  max-width: 40rem;
+  border-radius: 4px;
+`
+export const First = styled.div`
+margin-top: 1rem;
+padding: 2.5rem 2.5rem 2rem;
+background-color: ${props => props.theme["base-card"]};
+border-radius: 4px;
+`
+export const Second = styled.div`
+margin-top: 0.75rem;
+padding: 2.5rem 2.5rem 2rem;
+background-color: ${props => props.theme["base-card"]};
+border-radius: 4px;
 `
 export const HeaderForm = styled.div`
 margin-bottom: 2rem;
@@ -34,11 +44,16 @@ p:first-child{
     padding-left: 1.875rem;
   }
 `
-export const AdressForm = styled.form`
+export const AdressForm = styled.div`
   
-
+  p{
+    display: flex;
+    gap: 0.75rem;
+  }
   input{
+    display: inline-block;
     background-color: ${props=> props.theme["base-input"]};
+    width: 12.5rem;
     border: none;
     height: 2.625rem;
     margin-bottom: 1rem;
@@ -47,7 +62,79 @@ export const AdressForm = styled.form`
     color: ${props=>props.theme["base-text"]};
     border-radius: 6px;
   }
+  p:nth-child(2){
+    input{
+      width: 100%;
+    }
+  }
+    p:nth-child(3){
+      input:first-child{
+      width: 12.5rem;
+    }
+    input:last-child{
+      width: calc(100% - 13rem);
+    }
+  }
+  p:last-child{
+    
+    input:first-child{
+     max-width: 12.5rem;
+    }
+    input:last-child{
+      width: 3.75rem;
+    }
+  }
+
+
+
   input::placeholder{
     color: ${props=> props.theme["base-label"]};
   }
+`
+export const PaymentForm = styled.div`
+padding-bottom: 2rem;
+ p{
+  display:flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  span:first-child{
+    color: ${props=> props.theme["purple"]};
+    font-size: 1rem;
+  }
+  span:last-child{
+    color: ${props=> props.theme["base-subtitle"]};
+    font-size: 1rem;
+  }
+ }
+ p:last-child{
+  color: ${props=> props.theme['base-text']};
+  font-size: 0.825rem;
+  padding-left: 1.875rem;
+ }
+`
+export const TypePayment = styled.div`
+    display: flex;
+    color: ${props=> props.theme['base-text']};
+    flex: 1 1 0px;
+
+    span{
+      color: ${props=> props.theme['purple']};
+    }
+    label{
+      display: flex;
+      padding: 1rem;
+      gap: 0.75rem;
+      align-items: center;
+      font-size: 0.7rem;
+      background-color: ${props=> props.theme['base-button']};
+      margin-right: 0.75rem;
+      border-radius: 6px;
+    }
+    input{
+      /* visibility: hidden; */
+    }
+    input[type="radio"]:checked+label{
+      background-color: red;
+    }
 `
